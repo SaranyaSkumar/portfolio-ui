23293eca864661e1db3703b5507e879607398e34
Saranya <saranya.k@loomy.com>
2024-01-06 16:28:33 +1300

    err fixed

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,12 +1,12 @@
 // components/AboutMe.js
 const AboutMe = () => {
   return (
-    <div className="bg-white text-gray-800">
+    <div className="abg-white text-gray-800">
       <div className="p-10">
         <h1 className="text-3xl font-bold">About Me</h1>
         <div className="mt-2 mb-6 h-1 w-24 bg-red-500"></div>
         <p className="text-lg">
-        I'm Saranya Sasi Kumar, a full-stack developer specializing in NodeJS, ReactJS, Angular, PostgreSQL, and Sequelize. I excel in turning complex problems into simple, elegant designs. Dedicated to constant learning, I'm pursuing a Postgraduate Diploma in IT. My interests in drawing enhance my creativity and contribute to my holistic approach to development and design.
+        I&apos;m Saranya Sasi Kumar, a full-stack developer specializing in NodeJS, ReactJS, Angular, PostgreSQL, and Sequelize. I excel in turning complex problems into simple, elegant designs. Dedicated to constant learning, I&apos;m pursuing a Postgraduate Diploma in IT. My interests in drawing enhance my creativity and contribute to my holistic approach to development and design.
         </p>
       </div>
       <div className="flex justify-around flex-wrap p-10">
